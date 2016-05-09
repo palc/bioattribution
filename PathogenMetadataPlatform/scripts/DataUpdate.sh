f15d45646bbffec7259c3fb8615b7c5344677288
wchangmitre <wchang@mitre.org>
2016-05-08 20:00:54 -0700

    Updated readme and updated DataUpdate
    Updated readme with more details. Updated DataUpdate.sh to handle
    directories with too many files for the command line.

--- PathogenMetadataPlatform/scripts/DataUpdate.sh
+++ PathogenMetadataPlatform/scripts/DataUpdate.sh
@@ -5,7 +5,9 @@ export CLASSPATH=$CLASSPATH:${javapath}:${javapath}/postgresql-9.1-902.jdbc4.jar
 
 # Updating BioProject and BioSample records in the Database
 java -cp ${javapath}:${javapath}/postgresql-9.1-902.jdbc4.jar:${javapath}/xom-1.2.8.jar biosampleparser.BioSampleParser rebuilddb ${scriptpath}/config.properties
-java -cp ${javapath}:${javapath}/postgresql-9.1-902.jdbc4.jar:${javapath}/xom-1.2.8.jar biosampleparser.BioSampleParser loadnewdata `ls -1 ${biosamplefilepath}/FtpBioSample*.xml | wc -l` "${biosamplefilepath}/FtpBioSample" `ls -1 ${bioprojectfilepath}/FtpBioProject*.xml | wc -l` "${bioprojectfilepath}/FtpBioProject" "${idMapDir}/FilteredMappedIDs.out" "${scriptpath}/geomap.tsv" ${scriptpath}/config.properties >& ${javapath}/error.out
+countbiosamples=`ls -1 ${biosamplefilepath}/ | egrep "FtpBioSample" | wc -l`
+countbioprojects=`ls -1 ${bioprojectfilepath}/ | egrep "FtpBioProject" | wc -l` 
+java -cp ${javapath}:${javapath}/postgresql-9.1-902.jdbc4.jar:${javapath}/xom-1.2.8.jar biosampleparser.BioSampleParser loadnewdata ${countbiosamples} "${biosamplefilepath}/FtpBioSample" ${countbioprojects} "${bioprojectfilepath}/FtpBioProject" "${idMapDir}/FilteredMappedIDs.out" "${scriptpath}/geomap.tsv" ${scriptpath}/config.properties >& ${javapath}/error.out
 
 mv ${scriptpath}/nohup.out ${scriptpath}/nohup.txt
 rm ${scriptpath}/*.out
