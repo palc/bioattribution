383594c968824981f3b4abf6ac2e61eba011bba5
Christopher Garay <cgaray@mitre.org>
2016-07-03 18:51:01 -0400

    optimize calls to eutils

--- scripts/mapBioSampleBioProjectIDs.py
+++ scripts/mapBioSampleBioProjectIDs.py
@@ -1,36 +1,51 @@
+import requests
 import xml.dom.minidom
 from xml.parsers.expat import ExpatError
 import os
+import os.path
 import sys
 
-path = sys.argv[1];
-path = path[1:-1];
-os.system('rm -f '+ path +'MappedIDs.out')
-xmlIndex = open(path +'PRJNA_parsedID.out').readlines()
-outfile = open(path + 'MappedIDs.out', 'a')
-firstline = 0
+import pdb
 
+def batch(iterable, n=1):
+    l = len(iterable)
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
 
-for i in range (firstline,len(xmlIndex)):
-        BioProjectID = str(xmlIndex[i][0:len(xmlIndex[i])-1])
+mappath = sys.argv[1];
+mappath = mappath[1:-1];
 
-        os.system("wget -q -O "+ path +"BioProject" + BioProjectID + ".xml " + """"http://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=bioproject&db=biosample&id=""" + BioProjectID + """" """)
+scriptpath = sys.argv[2];
+scriptpath = scriptpath[1:-1];
 
-for j in range (firstline,len(xmlIndex)):
-        BioProjectID = str(xmlIndex[j][0:len(xmlIndex[j])-1])
-        XMLfile = path + "BioProject" + BioProjectID + ".xml"
-        if os.path.isfile(XMLfile) == True:
-            try:
-                dom = xml.dom.minidom.parse(XMLfile)
-                Link = dom.getElementsByTagName('Link')
-                k = 0
-                for k in range(0, len(Link)):
-                        Id = Link[k].getElementsByTagName('Id')[0].firstChild.nodeValue
-                        MappedIDs = "BioProject" + BioProjectID + "\tBioSample" + str(Id) + "\n"
+os.system('rm -f '+ mappath +'MappedIDs.out')
+xmlIndex = [index.strip() for index in open(mappath +'PRJNA_parsedID.out').readlines()]
+outfile = open(os.path.join(mappath, 'MappedIDs.out'), 'a')
+biosample_out = open(os.path.join(scriptpath, 'BioSampleIDs.out'), 'aw')
+
+for id_batch in batch(xmlIndex, 200):
+    elink_url = """http://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?retmod=json&dbfrom=bioproject&db=biosample&{}"""
+    query = '&'.join(['id={}'.format(index) for index in id_batch])
+    response = requests.post(elink_url.format(query))
+
+    if response.status_code == 200:
+        try:
+            dom = xml.dom.minidom.parseString(response.text)
+            LinkSets = dom.getElementsByTagName('LinkSet')
+            for LinkSet in LinkSets:
+                BioProjectID = LinkSet.getElementsByTagName('Id')[0].firstChild.nodeValue
+                Links = LinkSet.getElementsByTagName('Link')
+                seenIds = []
+                for Link in Links:
+                    Id = Link.getElementsByTagName('Id')[0].firstChild.nodeValue
+                    if not Id in seenIds:
+                        MappedIDs = "BioProject" + BioProjectID + "\tBioSample" + Id + "\n"
+
                         outfile.write(MappedIDs)
-            except ExpatError:
-                print "Ignoring "+BioProjectID
+                        biosample_out.write(Id + "\n")
+
+                        seenIds.append(Id)
+        except ExpatError:
+            print "Ignoring " + BioProjectID
 outfile.close()
-
-
-
+biosample_out.close()
