ba3e68bfe1ac52958692c1bdf65b1041e5288845
Christopher Garay <cgaray@mitre.org>
2016-07-05 08:09:44 -0400

    iterate over biosample files only once to map IDs

--- scripts/DataMapping.sh
+++ scripts/DataMapping.sh
@@ -2,19 +2,18 @@
 set -eux -o pipefail
 
 . config.properties
-rm ${idMapDir}/*appedIDs.out
-rm ${scriptpath}/filter.awk
+rm -f ${idMapDir}/*appedIDs.out
+rm -f ${scriptpath}/filter.awk
 
 
 # Mapping of BioProject and BioSample IDs
-rm ${idMapDir}/PRJNA_*.out
+rm -f ${idMapDir}/PRJNA_*.out
 grep '<ArchiveID accession="PRJNA' ${bioprojectfilepath}/bioproject.xml > ${idMapDir}/PRJNA_ID.out
 awk -F "\"" '{print $6;}' ${idMapDir}/PRJNA_ID.out > ${idMapDir}/PRJNA_parsedID.out
-python ${scriptpath}/mapBioSampleBioProjectIDs.py "\"${idMapDir}/\""
+python ${scriptpath}/mapBioSampleBioProjectIDs.py "\"${idMapDir}/\"" "\"${scriptpath}\""
 
 # Remove BioSample IDs that are not associated with BioProject IDs
 awk -v scriptpath="\"${scriptpath}\"" -f ${scriptpath}/getIds.awk ${biosamplefilepath}/biosample_set.xml > ${scriptpath}/idlist.out
-python ${scriptpath}/getBioSampleID.py "\"${idMapDir}/\"" "\"${scriptpath}/\""
 sort -nu ${scriptpath}/BioSampleIDs.out > ${scriptpath}/sorted_BioSampleIDs.out
 sort -nu ${scriptpath}/idlist.out > ${scriptpath}/sorted_idlist.out
 diff ${scriptpath}/sorted_BioSampleIDs.out ${scriptpath}/sorted_idlist.out > ${scriptpath}/diff_IDs.out
