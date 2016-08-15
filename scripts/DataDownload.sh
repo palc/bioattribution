de8c707507c2032c031e4c0549459bf16524113d
Christopher Garay <cgaray@mitre.org>
2016-08-15 09:26:44 -0400

    use curl for download

--- scripts/DataDownload.sh
+++ scripts/DataDownload.sh
@@ -3,11 +3,10 @@ set -eux -o pipefail
 . config.properties
 
 # Download BioSample XML
-rm ${biosamplefilepath}/biosample_set.xml
-wget -O ${biosamplefilepath}/biosample_set.xml.gz ftp://ftp.ncbi.nlm.nih.gov/biosample/biosample_set.xml.gz
+rm -f ${biosamplefilepath}/biosample_set.xml
+curl -k -o ${biosamplefilepath}/biosample_set.xml.gz ftp://ftp.ncbi.nlm.nih.gov/biosample/biosample_set.xml.gz
 gunzip ${biosamplefilepath}/biosample_set.xml.gz
 
 # Download BioProject XML
-rm ${bioprojectfilepath}/bioproject.xml
-wget -O ${bioprojectfilepath}/bioproject.xml ftp://ftp.ncbi.nlm.nih.gov/bioproject/bioproject.xml
-
+rm -f ${bioprojectfilepath}/bioproject.xml
+curl -o ${bioprojectfilepath}/bioproject.xml ftp://ftp.ncbi.nlm.nih.gov/bioproject/bioproject.xml
